6f8b96bdf136e09e0a8e1d6794958ede11d53676
Connor Mentel <connor.mentel@gmail.com>
2020-07-21 17:44:50 -0400

    Updated immutable winning

--- RockPaperScissors/rock_paper_scissors.py
+++ RockPaperScissors/rock_paper_scissors.py
@@ -1,6 +1,6 @@
 '''
 Rock Paper Scissors
-Last mod: 7/18/20
+Last mod: 7/21/20
 '''
 
 
@@ -20,9 +20,9 @@ def play_game(games, player_one, player_two, player_one_wins, player_two_wins, g
     while games <= games_to_play:
         print("GAME:", games)
         print(player_one, "what's your hand?")
-        player_one_guess = input().upper()
+        player_one_guess = input().strip().upper()
         print(player_two, "what's your guess?")
-        player_two_guess = input().upper()
+        player_two_guess = input().strip().upper()
 
         # logic handing outcome of players
         if player_one_guess == 'ROCK':
@@ -74,11 +74,15 @@ def play_game(games, player_one, player_two, player_one_wins, player_two_wins, g
         # prints games played and current score
         print(print_score(player_one, player_two, player_one_wins, player_two_wins))
 
+        # if outcome is immutable, break loop
+        if player_two_wins > games_to_play / 2 or player_two_wins > games_to_play / 2:
+            break
+
     # prints name of winner or if there was a tie
     get_winner(player_one, player_two, player_one_wins, player_two_wins)
 
     # asks to play again ?
-    if play_again(games_to_play) == True:
+    if play_again(player_one_wins + player_two_wins) == True:
         initialize()
     else:
         exit("Thanks for playing!\n")
@@ -86,39 +90,41 @@ def play_game(games, player_one, player_two, player_one_wins, player_two_wins, g
 
 def get_winner(player_one, player_two, player_one_wins, player_two_wins):
     if player_one_wins > player_two_wins:
-        print(player_one, "wins!\n", print_score(player_one, player_two, player_one_wins, player_two_wins))
+        print(player_one, "wins!\n\n", print_score(player_one, player_two, player_one_wins, player_two_wins))
     elif player_two_wins > player_one_wins:
-        print(player_two, "wins!\n", print_score(player_one, player_two, player_one_wins, player_two_wins))
+        print(player_two, "wins!\n\n", print_score(player_one, player_two, player_one_wins, player_two_wins))
     else:
         print("It's a tie!!\nGood game", player_one, "and", player_two, "\n")
 
 
 def print_score(player_one, player_two, player_one_wins, player_two_wins):
-    score_string = "SCORE:\n"
     player_one_string = player_one + " has " + str(player_one_wins)
     player_two_string = player_two + " has " + str(player_two_wins)
 
     if player_one_wins == 1 and player_two_wins == 1:
-        return score_string + player_one_string + " win \n" + player_two_string + " win\n"
+        return "SCORE:\n" + player_one_string + " win \n" + player_two_string + " win\n"
 
     elif player_one_wins == 1 and player_two_wins != 1:
-        return score_string + player_one_string + " win \n" + player_two_string + " wins\n"
+        return "SCORE:\n" + player_one_string + " win \n" + player_two_string + " wins\n"
 
     elif player_one_wins != 1 and player_two_wins == 1:
-        return score_string + player_one_string + " wins \n" + player_two_string + " win\n"
+        return "SCORE:\n" + player_one_string + " wins \n" + player_two_string + " win\n"
     else:
-        return score_string + player_one_string + " wins \n" + player_two_string + " wins\n"
+        return "SCORE:\n" + player_one_string + " wins \n" + player_two_string + " wins\n"
 
 
-def play_again(games_to_play):
-    print("That's",games_to_play,"games!")
-    option = input("Would you want to play again?\n")
+def play_again(games):
+    if games == 1:
+        print("That's", games, "game!")
+    else:
+        print("That's",games,"games!")
+    option = input("Would you want to play again? ('YES' or 'NO')\n")
     if option.upper() == "YES":
         return True
     elif option.upper() == "NO":
         return False
     else:
-        play_again(games_to_play)
+        play_again(games)
 
 
 initialize()
